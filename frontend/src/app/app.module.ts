6d2af33fd72bf81868043e9d13b2d891a03889b4
Deepank kartikey <deepankkartikey@gmail.com>
2023-01-22 00:56:03 -0500

    Creating Admin UI

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -25,6 +25,8 @@ import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboa
 import { ProfileComponent } from './pages/profile/profile.component';
 import {MatListModule} from '@angular/material/list';
 import { SidebarComponent } from './pages/admin/sidebar/sidebar.component';
+import { WelcomeComponent } from './pages/admin/welcome/welcome.component';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -37,7 +39,8 @@ import { SidebarComponent } from './pages/admin/sidebar/sidebar.component';
     DashboardComponent,
     UserDashboardComponent,
     ProfileComponent,
-    SidebarComponent
+    SidebarComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
@@ -52,7 +55,8 @@ import { SidebarComponent } from './pages/admin/sidebar/sidebar.component';
     MatCardModule,
     MatToolbarModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatTableModule
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginService } from 'src/app/services/login.service';
+
+type User = {
+  id: string;
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  authorities: [];
+  enabled: boolean;
+}
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  
+  user: User | undefined;
+  constructor(private login: LoginService) { }
 
   ngOnInit(): void {
+    // called as soon as component is loaded to initialize
+    this.user = this.login.getUser();
   }
 
 }
