2169f1951207037801fbd65e2b5e3115b7ee80a9
Deepank kartikey <deepankkartikey@gmail.com>
2023-03-07 23:56:08 -0500

    Update Quiz functionality

---
+++ frontend/src/app/pages/admin/update-quiz/update-quiz.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CategoryService } from 'src/app/services/category.service';
+import { QuizService } from 'src/app/services/quiz.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-update-quiz',
+  templateUrl: './update-quiz.component.html',
+  styleUrls: ['./update-quiz.component.css']
+})
+export class UpdateQuizComponent implements OnInit {
+
+  constructor(private _route: ActivatedRoute, private _quiz: QuizService,
+    private _category: CategoryService) { }
+
+  qId = 0;
+  quiz: any;
+  categories: any;
+
+  ngOnInit(): void {
+    // fetch currently clicked quizId using ActivatedRoute
+    this.qId = this._route.snapshot.params['qid'];
+    // alert(this.qId);
+
+    // fetch quiz data for selected quizId
+    this._quiz.getQuiz(this.qId).subscribe(
+      (data: any)=>{
+        this.quiz = data;
+        console.log(this.quiz)
+      },
+      (error)=>{
+        console.log(error)
+      }
+    )
+
+    // load categories data for dropdown
+    this._category.categories().subscribe(
+      (data: any)=>{
+        this.categories = data
+      },
+      (error)=>{
+        console.log(error)
+      }
+    ) 
+  }
+
+  // update on form submit
+  public updateQuiz(){
+    this._quiz.updateQuiz(this.quiz).subscribe(
+      (data: any)=>{
+        Swal.fire("Success !!", "Quiz updated", "success")
+      },
+      (error)=>{
+        Swal.fire("Error", "Error in Updating Quiz", "error")
+      }
+    )
+  }
+
+}
--- frontend/src/app/services/quiz.service.ts
+++ frontend/src/app/services/quiz.service.ts
@@ -13,11 +13,21 @@ export class QuizService {
     return this._http.get(`${baseUrl}/quiz/`)
   }
 
+  // get single quiz
+  public getQuiz(quizId: any){
+    return this._http.get(`${baseUrl}/quiz/${quizId}`)
+  }
+
   public addQuiz(quizData: any){
     return this._http.post(`${baseUrl}/quiz/`, quizData);
   }
 
   public deleteQuiz(quizId: any){
     return this._http.delete(`${baseUrl}/quiz/${quizId}`)
+  }
+
+  // update quiz
+  public updateQuiz(quiz: any){
+    return this._http.put(`${baseUrl}/quiz/`, quiz)
   }
 }
